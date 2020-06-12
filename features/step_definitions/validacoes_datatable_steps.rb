bf2275df8fe1d2afb371c3a6c4c11a540b116b2f
cruz.eshilane <cruz.eshilane@cwi.com.br>
2020-06-12 00:03:02 -0300

    Atualização implementação external data

--- features/step_definitions/validacoes_datatable_steps.rb
+++ features/step_definitions/validacoes_datatable_steps.rb
@@ -1,6 +1,14 @@
+require 'csv'
+
 Dado('que estou na página inicial') do
     @home_page = HomePage.new
     @home_page.load
+end
+
+Entao('os links para todas as midias sociais devem ser exibidos') do
+    CSV.foreach("features/support/data/links.csv", headers: true) do |row|
+        expect(@home_page.send("exists_#{row['links']}_link?")).to be true
+    end
 end
 
 Entao('deve ser exibido o link para:') do | table |
